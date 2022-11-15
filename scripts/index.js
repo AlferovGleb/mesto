4ffa62d3473326c2718a58d91d0579c33993ad0d
Gleb Alferov <gleb.alferov23@yandex.ru>
2022-11-16 02:17:00 +0500

    index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,8 @@ const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 const popupSubmitButtonElement = popupElement.querySelector('.popup__submit-button');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const likeDisableButtonElement = document.querySelector('.element__like');
+const formElement = document.querySelector('.');
 
 const openPopup = function() {
   popupElement.classList.add('popup__is-opened');
@@ -12,12 +14,22 @@ const closePopup = function() {
 }
 
 const closePopupByClickOnOverlay = function(event) {
+  console.log(event);
   if (event.target !== event.currentTarget) {
     return;
   }
   closePopup();
 }
 
+const activeLike = function() {
+  likeDisableButtonElement.classList.add('element__like_active');
+}
+
+const disableLike = function() {
+  likeDisableButtonElement.classList.remove('element__like_active');
+}
+
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
-popupElement.addEventListener('click', closePopupByClickOnOverlay)
+popupElement.addEventListener('click', closePopupByClickOnOverlay);
+likeDisableButtonElement.addEventListener('click', activeLike);
