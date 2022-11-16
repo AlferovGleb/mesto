090598d78bd355f4cc4b9a55026dae92e75985ae
Gleb Alferov <gleb.alferov23@yandex.ru>
2022-11-16 20:12:04 +0500

    README

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,16 @@ const popupCloseButtonElement = popupElement.querySelector('.popup__close-button
 const popupSubmitButtonElement = popupElement.querySelector('.popup__submit-button');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 const likeDisableButtonElement = document.querySelector('.element__like');
-const formElement = document.querySelector('.');
+const formElement = popupElement.querySelector('.popup__form');
+const nameInput = popupElement.querySelector('.popup__form-name');
+const textInput = popupElement.querySelector('.popup__form-text');
+const profileName = document.querySelector('.profile__name');
+const profileText = document.querySelector('.profile__text');
 
 const openPopup = function() {
   popupElement.classList.add('popup__is-opened');
+  nameInput.value = profileName.textContent;
+  textInput.value = profileText.textContent;
 }
 
 const closePopup = function() {
@@ -29,7 +35,15 @@ const disableLike = function() {
   likeDisableButtonElement.classList.remove('element__like_active');
 }
 
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileText.textContent = textInput.value;
+  closePopup();
+}
+
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 popupElement.addEventListener('click', closePopupByClickOnOverlay);
 likeDisableButtonElement.addEventListener('click', activeLike);
+formElement.addEventListener('submit', formSubmitHandler);
