8fca3471f6a88812f581e63b400ea928c7d93918
Gleb Alferov <gleb.alferov23@yandex.ru>
2022-11-19 21:46:35 +0500

    final2

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,19 @@ const popupCloseButtonElement = popupElement.querySelector('.popup__close-button
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 const likeDisableButtonElement = document.querySelector('.element__like');
 const formElement = popupElement.querySelector('.popup__form');
-const nameInput = popupElement.querySelector('.popup__input_name');
-const textInput = popupElement.querySelector('.popup__input_text');
+const nameInput = popupElement.querySelector('.popup__input_type_name');
+const textInput = popupElement.querySelector('.popup__input_type_text');
 const profileName = document.querySelector('.profile__name');
 const profileText = document.querySelector('.profile__text');
 
 const openPopup = function() {
-  popupElement.classList.add('popup__opened');
+  popupElement.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   textInput.value = profileText.textContent;
 }
 
 const closePopup = function() {
-  popupElement.classList.remove('popup__opened');
+  popupElement.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
